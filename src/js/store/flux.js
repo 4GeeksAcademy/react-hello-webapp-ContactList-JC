4e1f0499866549d0c3877ad1faabc241d8c5bfd1
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-01-23 17:13:27 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,24 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    // almacenamiento de variables (key: valor)
+    store: {
+      API: "https://playground.4geeks.com/apis/fake/contact/",
+      Contacts: [],
+      datos: ["juan", "marieth", "nicole"],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      getAllContact: async () => {
+        const store = getStore();
+        const response = await fetch(store.API + "agenda/Juan_contact");
+        const getContacts = await response.json();
+        setStore({ Contacts: getContacts });
+      },
+    },
+  };
 };
 
 export default getState;
